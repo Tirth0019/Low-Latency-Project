6983cf1c61ed80edefad5ffe369ae58452304797
Tirth0019 <tirthpatel618@gmail.com>
2026-05-16 00:59:39 +0530

    feat(metrics): serve dashboard.html directly to avoid mixed content

--- src/metrics/http_server.cpp
+++ src/metrics/http_server.cpp
@@ -2,6 +2,8 @@
 #include "net/socket.hpp"
 #include <cstring>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 
 namespace metrics {
@@ -22,24 +24,46 @@ void HttpServer::run(uint16_t port) {
       continue;
     }
 
-    char buf[512] = {};
+    char buf[1024] = {};
     client.recv(buf, sizeof(buf));
 
-    std::string json =
-        "{\"p50_ns\":" + std::to_string(tracker_.get_p50()) +
-        ",\"p99_ns\":" + std::to_string(tracker_.get_p99()) +
-        ",\"p999_ns\":" + std::to_string(tracker_.get_p999()) +
-        ",\"orders_per_sec\":" + std::to_string(order_count_.load()) +
-        ",\"uptime_sec\":" + std::to_string(this->uptime_sec_) + "}";
+    std::string request(buf);
+    std::string response;
 
-    std::string response = "HTTP/1.1 200 OK\r\n"
-                           "Content-Type: application/json\r\n"
-                           "Access-Control-Allow-Origin: *\r\n"
-                           "Content-Length: " +
-                           std::to_string(json.size()) +
-                           "\r\n"
-                           "\r\n" +
-                           json;
+    if (request.find("GET /metrics") != std::string::npos) {
+      std::string json =
+          "{\"p50_ns\":" + std::to_string(tracker_.get_p50()) +
+          ",\"p99_ns\":" + std::to_string(tracker_.get_p99()) +
+          ",\"p999_ns\":" + std::to_string(tracker_.get_p999()) +
+          ",\"orders_per_sec\":" + std::to_string(order_count_.load()) +
+          ",\"uptime_seconds\":" + std::to_string(this->uptime_sec_) + "}";
+
+      response = "HTTP/1.1 200 OK\r\n"
+                 "Content-Type: application/json\r\n"
+                 "Access-Control-Allow-Origin: *\r\n"
+                 "Content-Length: " +
+                 std::to_string(json.size()) + "\r\n"
+                 "\r\n" +
+                 json;
+    } else {
+      // Default to serving dashboard.html for GET / or any other path
+      std::ifstream f("docs/dashboard.html");
+      if (f.good()) {
+        std::stringstream ss;
+        ss << f.rdbuf();
+        std::string html = ss.str();
+        response = "HTTP/1.1 200 OK\r\n"
+                   "Content-Type: text/html\r\n"
+                   "Content-Length: " +
+                   std::to_string(html.size()) + "\r\n"
+                   "\r\n" +
+                   html;
+      } else {
+        response = "HTTP/1.1 404 Not Found\r\n"
+                   "Content-Length: 0\r\n"
+                   "\r\n";
+      }
+    }
 
     client.send(response.c_str(), response.size());
   }
